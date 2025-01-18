6bb23ea78a5f66fdf1bcad6713c1bec68113157c
Mitch <mitchhuang777@gmail.com>
2025-01-18 17:51:20 +0800

    feat: example router and contains DB, log_manager DI

--- app/routers/example_router.py
+++ app/routers/example_router.py
@@ -1,10 +1,22 @@
 from fastapi import APIRouter
+from app.database import get_db
+from app.dependencies.log_dependency import get_log_manager
+from app.log_manager import LogManager
+from app.utils.time_utils import calculate_process_time
+from app.schemas.sample import SampleResponse
+from fastapi import APIRouter, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
 
-router = APIRouter()
+sample_router = APIRouter()
 
-@router.get("/sample")
-async def get_sample_data():
+@sample_router.get("/sample", response_model=SampleResponse)
+async def get_sample_data(
+    db: AsyncSession = Depends(get_db),
+    log_manager: LogManager = Depends(get_log_manager)
+):
     """
     簡單範例端點，返回靜態數據。
     """
-    return {"message": "Hello, this is a sample response!"}
+    return SampleResponse(
+        message="Hello, World!"
+    )
