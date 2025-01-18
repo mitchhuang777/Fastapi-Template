d3881808c0908791acc8bbad6dd42c34cac67573
Mitch <mitchhuang777@gmail.com>
2025-01-18 17:54:01 +0800

    fix: adjust return for DB_PASS exist and or not

--- app/database.py
+++ app/database.py
@@ -2,25 +2,26 @@ from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.sql import text
 from dotenv import load_dotenv
+from pathlib import Path
 import os
 
 # 動態加載環境變數文件
 ENV = os.getenv("ENV", "dev")  # 預設為開發環境
-dotenv_file = f".env.{ENV}"  # 根據環境名稱動態選擇 .env 文件
+dotenv_file = Path(__file__).parent / f".env.{ENV}"  # 根據環境名稱動態選擇 .env 文件
 load_dotenv(dotenv_path=dotenv_file)
 
 # 從環境變數中讀取資料庫連接參數
 def get_database_url() -> str:
     DB_HOST = os.getenv("DB_HOST")
     DB_USER = os.getenv("DB_USER")
-    DB_PASS = os.getenv("DB_PASS")
+    DB_PASS = os.getenv("DB_PASS", "")
     DB_PORT = os.getenv("DB_PORT")
     DB_NAME = os.getenv("DB_NAME")
-    # 驗證環境變數是否設置
-    if not all([DB_HOST, DB_USER, DB_PASS, DB_PORT, DB_NAME]):
-        raise ValueError("One or more required environment variables are missing. Please check .env.<ENV> file.")
+
+    if DB_PASS:
+        return f"mysql+aiomysql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
     
-    return f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+    return f"mysql+aiomysql://{DB_USER}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 # 動態生成 DATABASE_URL
 DATABASE_URL = get_database_url()
@@ -56,20 +57,20 @@ async def get_db() -> AsyncSession:
             await session.close()  # 確保會話關閉
 
 # 測試連線功能（可選）
-async def test_connection():
-    """
-    測試資料庫連線是否成功。
-    """
-    try:
-        async with engine.connect() as conn:
-            # 使用 SQLAlchemy 的 text() 包裹原始 SQL 語句
-            result = await conn.execute(text("SELECT 1"))
-            print("Database connection successful! Test query result:", result.scalar())
-    except Exception as e:
-        print("Database connection failed:", str(e))
-    finally:
-        await engine.dispose()
+# async def test_connection():
+#     """
+#     測試資料庫連線是否成功。
+#     """
+#     try:
+#         async with engine.connect() as conn:
+#             # 使用 SQLAlchemy 的 text() 包裹原始 SQL 語句
+#             result = await conn.execute(text("SELECT 1"))
+#             print("Database connection successful! Test query result:", result.scalar())
+#     except Exception as e:
+#         print("Database connection failed:", str(e))
+#     finally:
+#         await engine.dispose()
 
-# 如果需要在腳本模式下測試，可以取消以下註解
-import asyncio
-asyncio.run(test_connection())
+# # 如果需要在腳本模式下測試，可以取消以下註解
+# import asyncio
+# asyncio.run(test_connection())
